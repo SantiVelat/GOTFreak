437f425579fdd33bfcd074904715b843a0ae2dfd
Hernan de Soto <hdesoto@gmail.com>
2017-07-27 15:21:22 +0200

    Linted js files in Characters folder

--- app/routes/character/config.js
+++ app/routes/character/config.js
@@ -1,10 +1,11 @@
-angular.module('GOTFreak')
-   .config( function( $routeProvider ) {
+/* global angular */
 
-    $routeProvider
+angular.module('GOTFreak')
+   .config(function ($routeProvider) {
+     $routeProvider
       .when('/character/:slug', {
         templateUrl: 'app/routes/character/template.html',
         controller: 'characterController',
         controllerAs: 'ctrl'
       })
-  })
+   })
--- app/routes/character/controller.js
+++ app/routes/character/controller.js
@@ -1,16 +1,15 @@
-app.controller('characterController', function($routeParams, DataService) {
+/* global app */
 
+app.controller('characterController', function ($routeParams, DataService) {
   var self = this
-
   var slugCharacter = $routeParams.slug
   DataService.getDetailsCharacter(slugCharacter)
-    .then( function( response ) {
+    .then(function (response) {
       self.character = response.data.data
       DataService.getHouseEmblem(self.character.house)
-      	.then( function( responseTwo ){
-      		self.emblem=responseTwo.data.data.imageLink;
-      		console.log(self.emblem);
-      	})
+        .then(function (responseTwo) {
+          self.emblem = responseTwo.data.data.imageLink
+          console.log(self.emblem)
+        })
     })
-
 })
